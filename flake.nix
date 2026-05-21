fb49354f237dad72a3d106204793c91701668746
Spencer Dent <mr.spencerdent@gmail.com>
2026-05-21 12:42:59 -0400

    feat: init mdbook

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "RMS Inventory Project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          mdbook
+          mdbook-mermaid
+        ];
+      };
+    };
+}
